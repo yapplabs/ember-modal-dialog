ef3df184e126022b1eee1e23b20b1e0e39d2ae57
Luke Melia <luke@lukemelia.com>
2015-06-04 14:57:24 -0400

    Attempt to use hbs template appropriate to project's Ember version.

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
+var fs = require('fs');
+var semver = require('semver');
+var stew = require('broccoli-stew');
 
 module.exports = {
-  name: 'ember-modal-dialog'
+  name: 'ember-modal-dialog',
+  treeForAddonTemplates: function treeForAddonTemplates (tree) {
+    if (useLegacyBindingSyntax(this)) {
+      tree = stew.rm(tree, 'components/modal-dialog.hbs');
+      tree = stew.mv(tree, 'components/modal-dialog-legacy.hbs', 'components/modal-dialog.hbs');
+    } else {
+      tree = stew.rm(tree, 'components/modal-dialog-legacy.hbs');
+    }
+    return tree;
+  }
 };
+
+function useLegacyBindingSyntax(addon){
+  var emberVersion = getEmberVersion(addon);
+  if (/canary/.test(emberVersion)) { return false; }
+  if (/beta/.test(emberVersion)) { return false; }
+  if (/beta/.test(emberVersion)) { return false; }
+  return semver.lt(emberVersion, '1.13.0');
+}
+
+function getEmberVersion(addon) {
+  if (!addon.project) {
+    return null;
+  }
+
+  var bowerPath = path.join(addon.project.root, 'bower.json')
+  var bowerObj = JSON.parse(fs.readFileSync(bowerPath, 'utf8'));
+  return bowerObj.dependencies.ember;
+}
