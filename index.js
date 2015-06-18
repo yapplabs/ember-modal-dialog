b801599860d8fab909f0c3637f5f403a572c8ec4
Robert Jackson <robert.w.jackson@me.com>
2015-06-18 17:26:59 -0400

    Refactor multi-version template build pipeline.

--- index.js
+++ index.js
@@ -2,36 +2,22 @@
 'use strict';
 var path = require('path');
 var fs = require('fs');
-var semver = require('semver');
-var stew = require('broccoli-stew');
+
+var VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
   name: 'ember-modal-dialog',
+
   treeForAddonTemplates: function treeForAddonTemplates (tree) {
-    if (useLegacyBindingSyntax(this)) {
-      tree = stew.rm(tree, 'components/modal-dialog.hbs');
-      tree = stew.mv(tree, 'components/modal-dialog-legacy.hbs', 'components/modal-dialog.hbs');
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember', 'bower');
+
+    var baseTemplatesPath = path.join(this.root, 'addon/templates');
+
+    if (dep.satisfies('>= 1.13.0')) {
+      return this.treeGenerator(path.join(baseTemplatesPath, 'current'));
     } else {
-      tree = stew.rm(tree, 'components/modal-dialog-legacy.hbs');
+      return this.treeGenerator(path.join(baseTemplatesPath, 'lt-1-13'));
     }
-    return tree;
   }
 };
-
-function useLegacyBindingSyntax(addon){
-  var emberVersion = getEmberVersion(addon);
-  if (/canary/.test(emberVersion)) { return false; }
-  if (/beta/.test(emberVersion)) { return false; }
-  if (/beta/.test(emberVersion)) { return false; }
-  return semver.lt(emberVersion, '1.13.0');
-}
-
-function getEmberVersion(addon) {
-  if (!addon.project) {
-    return null;
-  }
-
-  var bowerPath = path.join(addon.project.root, 'bower.json')
-  var bowerObj = JSON.parse(fs.readFileSync(bowerPath, 'utf8'));
-  return bowerObj.dependencies.ember;
-}
