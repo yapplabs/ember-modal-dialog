9ca5c1804d2e8747196ae8fd7cb3455a8c60d520
Luke Melia <luke@lukemelia.com>
2016-07-12 22:27:03 -0400

    Update ember-cli and use ember try's versionCompatibility

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,61 +1,68 @@
 module.exports = {
-  scenarios: [
-    {
-      name: 'default',
-      dependencies: { }
-    },
-    {
-      name: '1.10.1',
-      dependencies: {
-        'ember': '1.10.1',
-        'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
-      }
-    },
-    {
-      name: '1.11.3',
-      dependencies: {
-        'ember': '1.11.3',
-        'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
-      }
-    },
-    {
-      name: '1.12.1',
-      dependencies: {
-        'ember': '1.12.1'
-      }
-    },
-    {
-      name: '1.13.10',
-      dependencies: {
-        'ember': '1.13.10'
-      }
-    },
-    {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    }
-  ]
+  useVersionCompatibility: true
+  // scenarios: [
+  //   {
+  //     name: 'default',
+  //     dependencies: { }
+  //   },
+  //   {
+  //     name: '1.10.1',
+  //     dependencies: {
+  //       'ember': '1.10.1',
+  //       'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
+  //     }
+  //   },
+  //   {
+  //     name: '1.11.3',
+  //     dependencies: {
+  //       'ember': '1.11.3',
+  //       'ember-load-initializers': 'ember-cli/ember-load-initializers#0.0.2'
+  //     }
+  //   },
+  //   {
+  //     name: '1.12.1',
+  //     dependencies: {
+  //       'ember': '1.12.1'
+  //     }
+  //   },
+  //   {
+  //     name: '1.13.10',
+  //     dependencies: {
+  //       'ember': '1.13.10'
+  //     }
+  //   },
+  //   {
+  //     name: '2.4.5',
+  //     dependencies: {
+  //       'ember': '2.4.5'
+  //     }
+  //   },
+  //   {
+  //     name: 'ember-release',
+  //     dependencies: {
+  //       'ember': 'components/ember#release'
+  //     },
+  //     resolutions: {
+  //       'ember': 'release'
+  //     }
+  //   },
+  //   {
+  //     name: 'ember-beta',
+  //     dependencies: {
+  //       'ember': 'components/ember#beta'
+  //     },
+  //     resolutions: {
+  //       'ember': 'beta'
+  //     }
+  //   },
+  //   {
+  //     name: 'ember-canary',
+  //     dependencies: {
+  //       'ember': 'components/ember#canary'
+  //     },
+  //     resolutions: {
+  //       'ember': 'canary'
+  //     }
+  //   }
+  // ]
 };
