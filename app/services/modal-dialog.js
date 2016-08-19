d707e1b298fdf6b910835c68f686571fa5afb3b4
Ben Limmer <ben@benlimmer.com>
2016-08-19 17:41:05 -0600

    Support unit and integration tests out of the box. This commit maintains the previous behavior where the initializer appends the destination element to the DOM, but allows for falling back to the #ember-testing div in the testing environment.
    This resolves #78 and resolves #125

--- app/services/modal-dialog.js
+++ app/services/modal-dialog.js
@@ -1,2 +1,21 @@
+import Ember from 'ember';
 import Service from 'ember-modal-dialog/services/modal-dialog';
-export default Service;
+import ENV from '../config/environment';
+
+const {
+  computed
+} = Ember;
+
+export default Service.extend({
+  destinationElementId: computed(function() {
+    /*
+      everywhere except test, this property will be overwritten
+      by the initializer that appends the modal container div
+      to the DOM. because initializers don't run in unit/integration
+      tests, this is a nice fallback.
+    */
+    if (ENV.environment === 'test') {
+      return 'ember-testing';
+    }
+  })
+});
