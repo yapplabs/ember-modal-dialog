17f569fafec0eb7b32f2b7ad454f49cacc77983d
Daniel Steshenko <luciferburn26@gmail.com>
2017-04-04 18:55:18 +0300

    Check for Ember version with new ember-cli-version-checker API. (#166)
    Check for Ember version with new ember-cli-version-checker API.

--- index.js
+++ index.js
@@ -11,22 +11,23 @@ module.exports = {
   init: function() {
     this._super.init && this._super.init.apply(this, arguments);
     var checker = new VersionChecker(this);
-    if (!checker.for('ember-cli', 'npm').isAbove('0.2.6')) {
+
+    if (!checker.forEmber().isAbove('0.2.6')) {
       console.warn("Warning: ember-modal-dialog requires ember-cli >= 0.2.6 "
-                   + "for support for the addon-templates tree, which allows "
-                   + "us to support various ember versions. Use an older "
-                   + "version of ember-modal-dialog if you are stuck on an "
-                   + "older ember-cli.");
+        + "for support for the addon-templates tree, which allows "
+        + "us to support various ember versions. Use an older "
+        + "version of ember-modal-dialog if you are stuck on an "
+        + "older ember-cli.");
     }
   },
 
   treeForAddonTemplates: function treeForAddonTemplates (tree) {
     var checker = new VersionChecker(this);
-    var dep = checker.for('ember', 'bower');
+    var emberVersion = checker.forEmber();
 
     var baseTemplatesPath = path.join(this.root, 'addon/templates');
 
-    if (dep.lt('1.13.0-beta.1')) {
+    if (emberVersion.lt('1.13.0-beta.1')) {
       return this.treeGenerator(path.join(baseTemplatesPath, 'lt-1-13'));
     } else {
       return this.treeGenerator(path.join(baseTemplatesPath, 'current'));
