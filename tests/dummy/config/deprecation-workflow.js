ccaa37a3428e96e7c3c35961af74d8a4800cf555
Chris Masters <chrismasters@gmail.com>
2019-05-22 14:28:58 +0200

    Silencing test deprecations using deprecation workflow

---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,7 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
+    { handler: "silence", matchId: "computed-property.override" }
+  ]
+};
