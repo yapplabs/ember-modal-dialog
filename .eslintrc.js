f6745cc2ecbf9543882e8652ae3e80eff955ef97
Chris Masters <chrismasters@gmail.com>
2019-05-22 15:35:47 +0200

    Upgrading dependencies (#277)
    * Forward compatible
    * Ember 3.9 with tests passing (jQuery deprecations)
    * Silencing test deprecations using deprecation workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,39 @@ module.exports = {
   env: {
     browser: true
   },
-  plugins: [
-    'ember'
-  ],
   rules: {
-    'ember/new-module-imports': 'error',
-    'ember/no-jquery': 'error'
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
