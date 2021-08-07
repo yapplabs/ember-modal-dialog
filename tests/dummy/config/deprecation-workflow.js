91f0dbfb091709a0a947010c8e9a18e42b6a3fb5
Robert Wagner <rwwagner90@gmail.com>
2021-08-06 23:25:20 -0400

    Explicitly disable jQuery

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -2,7 +2,6 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
     { handler: "silence", matchId: "computed-property.override" },
     { handler: "silence", matchId: "deprecated-run-loop-and-computed-dot-access" },
     { handler: "silence", matchId: "ember-global" },
